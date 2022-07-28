2f68020db39a17925a05cfbb0dfc6327a87d2224
oibekwe123 <33491018+oibekwe123@users.noreply.github.com>
2022-07-28 15:21:31 -0500

    Update jenkins-ubuntu-installation.sh

--- JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
+++ JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
@@ -5,14 +5,18 @@ sudo apt update   # Update the repositories
 sudo apt install openjdk-11-jdk
 java -version
 # Install Jenkins
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo dnf upgrade
-# Add required dependencies for the jenkins package
-sudo dnf install chkconfig java-devel
-sudo dnf install jenkins
-# Start Jenkins
-sudo systemctl daemon-reload  # To Register the Jenkins service 
+sudo apt update   # Update the repositories
+sudo apt install openjdk-11-jdk
+java -version
+# Install Jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+sudo systemctl enable jenkins
 sudo systemctl start jenkins
 systemctl status jenkins
+
